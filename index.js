5251bd1252c981dc52ae0dba5250a030603f53f7
Duc Phan <duclaptop@mgail.com>
2017-10-10 16:56:16 +0300

    Update layout

--- index.js
+++ index.js
@@ -9,21 +9,38 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 server.listen(3000);
 
-
+//step 1
 io.on("connection",function(socket)
 {
     console.log("there is a connection to server: "+ socket.id);
+    
     socket.on("disconnect",function(){
         console.log(socket.id + " : disconnection ")
     });
 
-    //server lang nghe tu client
+    //step 3 
+    //server (listen from ) lang nghe tu client
     socket.on("Client-send-data",function(data)
-{
-    console.log(data + socket.id);
-    // server gui tra lai data cho khach hang client
-    io.sockets.emit("Server-send-data", data+"that is a data from server");
-});
+        {
+            console.log(data + socket.id);
+
+            //step 4
+            io.sockets.emit("Server-send-back-color", data);
+
+            // server gui tra lai data cho ALL khach hang client
+            //io.sockets.emit("Server-send-data", data+ "that is a data from server");
+
+            //server send back data to all clients , exception the sender
+        // socket.broadcast.emit("Server-send-data", data+"du lieu");
+
+            //server send back the sender, only sender
+            //socket.emit("Server-send-data", data+"du lieu");
+
+            //choose socket to send back data, ex: A to B, or C to B...
+            // io.to("socketid").emit();
+
+
+        });
 });
 
 app.get("/",function(req,res)
